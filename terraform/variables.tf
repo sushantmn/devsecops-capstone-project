9dedef67b9e59f0c52c25ac235163048c0975ae0
Sushant Nagaramunnoli <sushantnm@outlook.com>
2026-03-06 12:42:57 +0000

    Save IaC security rules for Jenkins and SSH

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "DevSecOps-Capstone-RG"
+}
+
+variable "location" {
+  default = "southindia"
+}
+
+variable "vm_name" {
+  default = "Jenkins-Docker-VM"
+}
